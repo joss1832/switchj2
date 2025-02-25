87508cb42f0556638f8e244b9a5a4dfbfb66ca4e
Josue <21387996@uagro.mx>
2025-02-25 11:23:45 -0600

    switch ejercicio2

---
+++ js/main.js
@@ -0,0 +1,10 @@
+let dia= 'lunes'
+
+switch (dia) {
+    case 'lunes':
+    case 'martes':    
+        console.log ('tienes clases con cesar')
+    
+    default:
+        console.log('tienes clases pero no se con quien')
+}
